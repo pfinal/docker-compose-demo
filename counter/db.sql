3d6dd8e308d59c8a91efb2d899fbfd30550242f9
Ethan <nicesoft333@qq.com>
2019-03-19 17:33:56 +0800

    init

---
+++ counter/db.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS testdb;
+USE testdb;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `username` varchar(50) UNIQUE NOT NULL,
+  `password` varchar(50) NOT NULL DEFAULT '',
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `user` (`username`, `password`)
+VALUES
+    ('ethan', md5('111111')),
+    ('joe',   md5('111111'));
